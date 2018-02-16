89a56dc6e605bba2805ce1a6995a216468288af3
braukhoff87 <braukhoff87@gmail.com>
2018-02-16 15:34:16 -0500

    openId connect impl

---
+++ src/main/java/com/moxe/core/config/HomeController.java
@@ -0,0 +1,24 @@
+package com.moxe.core.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @since 2/15/18.
+ */
+@Controller
+public class HomeController {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping("/")
+    @ResponseBody
+    public final String home() {
+        final String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        logger.info(username);
+        return "Welcome, " + username;
+    }
+}
